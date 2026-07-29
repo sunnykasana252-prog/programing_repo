1d2f6deb69ff004d17816eb6dbdd644f2cc2d723
sunny_kasana <sunnykasana252@gmail.com>
2026-07-29 10:15:05 +0530

    Add files via upload

---
+++ callbyreference.cpp
@@ -0,0 +1,19 @@
+
+
+// pass by reference
+
+#include<bits/stdc++.h>
+using namespace std;
+int sum(int *, int *);
+int main(){
+int a,b,c;
+cout<<"  enter two no";
+cin>>a>>b;
+c=sum(&a,&b);
+cout<<c;
+return 0;
+}
+int sum(int *a, int *b){
+ int c=*a+*b;
+ return c;
+}
