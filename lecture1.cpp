3709e9760c425b18e492403bff8c4520603eb419
sunny_kasana <sunnykasana252@gmail.com>
2026-07-29 10:14:13 +0530

    Add files via upload

---
+++ lecture1.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+     
+// for(int i=0;i<3;i++){
+//     for(int j=0;j<3;j++){
+//         for(int k=0;k<3;k++){
+//             cout<<i<<" "<<j<<" "<<k<<" ";
+//         }
+//     }
+// }
+
+
+// binary search
+// int n=10;
+// int arr[]={12 ,14, 16, 17, 24, 25,33,37,48,59};
+// int start=0,end=n-1;
+
+// int key;
+// cin>>key;
+// while (start<=end){
+//      int mid=(start+end)/2;
+// if(arr[mid]==key){
+//     cout<<arr[mid]<<" the key is present at index "<<mid+1;
+//     break;
+// }
+// else if(arr[mid]<key){
+// start=mid+1;}
+// else{
+// end=mid-1;
+// }}
+
+// linear search
+// int n=10;
+// int arr[]={12 ,14, 16, 17, 24, 25,33,37,48,59};
+// int key;
+
+// cin>>key;
+// for(int i=0;i<n;i++){
+//     if(arr[i]==key){
+//       cout<<"found";
+//         return 0;
+//     }
+  
+// }
+ 
+//     cout<<"not found";
+
+
+// first occurence
+
+
+return 0;
+}
+
+
